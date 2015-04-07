509f65841e19629388e18d19060b25f5459baef6
Glen Murphy <glen@glenmurphy.com>
2015-04-06 20:17:00 -0700

    Clean up clean up everybody clean up

--- app/src/main/java/com/glenmurphy/linepaper/LinepaperService.java
+++ app/src/main/java/com/glenmurphy/linepaper/LinepaperService.java
@@ -1,14 +1,11 @@
 package com.glenmurphy.linepaper;
 
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Handler;
 import android.service.wallpaper.WallpaperService;
-import android.util.Log;
 import android.view.SurfaceHolder;
 
-import java.io.IOException;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -85,11 +82,7 @@ public class LinepaperService extends WallpaperService {
 
       Calendar cal = GregorianCalendar.getInstance();
       cal.setTime(new Date());
-      /*
-      float angle = cal.get(Calendar.HOUR_OF_DAY) * 30f +  // 12 * 30 = 360  (degrees per day)
-                    cal.get(Calendar.MINUTE) / 2f +        // 60 / 2 = 30    (degrees per hour)
-                    cal.get(Calendar.SECOND) / 120f;       // 60 / 120 = 0.5 (degrees per minute)
-       */
+
       float angle = cal.get(Calendar.MINUTE) * 6f +         // 60 * 6 = 360       (degrees per hour)
                     cal.get(Calendar.SECOND) / 10f +        // 60 / 10 = 6        (degrees per minute)
                     cal.get(Calendar.MILLISECOND) / 10000f; // 1000 / 10000 = 0.1 (degrees per second)
@@ -104,7 +97,7 @@ public class LinepaperService extends WallpaperService {
         canvas.save();
         canvas.translate(i * barWidth * 2 + offset, -barLength / 2);
         float percentage = (i + numBars / 2) / (float)numBars;
-        //foreground.setAlpha(96 + (int)((i + numBars / 2) / (float)numBars * 128));
+
         foreground.setARGB(255,
            (int)(0x26 + (0x37 - 0x26) * (0.25 + percentage * 0.75)),
            (int)(0x32 + (0x47 - 0x32) * (0.25 + percentage * 0.75)),
